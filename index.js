1cf3f2230422160d4265eca2c68824fe4f4abd5b
samuel-joly <samuel.joly@laplateforme.io>
2021-05-31 13:13:02 +0200

    hook on push test

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-< 
+require('custom-env').env('dev')
+require('./bot/init_bot.js')
+const {discordBot} = require('./controllers/')
+const express = require('express');
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT;
+const host = process.env.HOST
+app.listen(port,host, () => {
+    console.log('listenening to http://'+host+':'+port);
+})
+
+app.get('/', (req, res) => {
+    res.json({
+        message:'Index.js'
+    })
+})
+
+app.post('/hookers', (req, res) => {
+    console.log(req.body);
+});
+
+app.use('/discord', discordBot)
